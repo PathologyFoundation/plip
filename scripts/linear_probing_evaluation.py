ce0fa62b53a0ab195b178ba1b986d34af7f65617
vinid <chiccobia@gmail.com>
2023-02-15 15:01:30 -0800

    adding file saving

--- scripts/linear_probing_evaluation.py
+++ scripts/linear_probing_evaluation.py
@@ -8,17 +8,17 @@ from evaluation.linear_probing.linear_classifier import LinearProber
 import pandas as pd
 from dotenv import load_dotenv
 import os
+from utils.results_handler import ResultsHandler
 
 def config():
     load_dotenv("../config.env")
 
-    DEFAULT_BACKBONE = os.environ["PC_DEFAULT_BACKBONE"]
 
     #DEFAULT_BACKBONE = "/oak/stanford/groups/jamesz/fede/medical_clip/novel_models_for_path/epoch_3_2023-01-30 14:57:58.402744_prime_bracket_4833.pt"
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", default="plip", type=str)
-    parser.add_argument("--backbone", default=DEFAULT_BACKBONE, type=str)
+    parser.add_argument("--backbone", default='default', type=str)
     parser.add_argument("--dataset", default="kather", type=str)
     parser.add_argument("--batch-size", default=128, type=int)
     parser.add_argument("--num-workers", default=4, type=int)
@@ -34,6 +34,9 @@ if __name__ == "__main__":
     args = config()
     data_folder = os.environ["PC_EVALUATION_DATA_ROOT_FOLDER"]
 
+    if args.model_name == "plip" and args.backbone == "default":
+        args.backbone = os.environ["PC_DEFAULT_BACKBONE"]
+
     train_dataset_name = args.dataset + "_train.csv"
     test_dataset_name = args.dataset + "_test.csv"
 
@@ -45,8 +48,14 @@ if __name__ == "__main__":
     train_x = embedder.image_embedder(train_dataset["images"].tolist(), additional_cache_name=train_dataset_name)
     test_x = embedder.image_embedder(test_dataset["images"].tolist(), additional_cache_name=test_dataset_name)
 
-    prober = LinearProber(alpha=args.alpha)
+    prober = LinearProber(alpha=args.alpha, seed=args.seed)
 
     results = prober.train_and_test(train_x, train_y=train_dataset["labels"].tolist(),
                                     test_x=test_x, test_y=test_dataset["labels"].tolist())
 
+    additional_parameters = {'dataset': args.dataset, 'seed': args.seed,
+                             'model': args.model_name, 'backbone': args.backbone,
+                             'alpha': args.alpha}
+
+    rs = ResultsHandler(args.dataset, "linear_probing", additional_parameters)
+    rs.add(results)
---
+++ utils/results_handler.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+
+class ResultsHandler:
+
+    def __init__(self, dataset, task, storing_params: dict):
+        """
+
+        :param storing_params: Dict of additional parameters we want to save in the csv
+        """
+        self.dataset = dataset
+        self.task = task
+        self.storing_params = storing_params
+
+    def add(self, results):
+
+        for record in results:
+            record.update(self.storing_params)
+
+        results_save_folder = os.environ["PC_RESULTS_FOLDER"]
+        results_file = os.path.join(results_save_folder, f"extended_results_{self.task}_{self.dataset}.csv")
+        df = pd.DataFrame(results)
+        if os.path.exists(results_file):
+            all_df = pd.read_csv(results_file, index_col=0)
+            all_df = pd.concat([all_df, df])
+            all_df.to_csv(results_file)
