a479aebc0d9b866cf8b2a4421eda000f0201c593
vinid <chiccobia@gmail.com>
2023-02-15 12:41:45 -0800

    fix shape of target vars

--- evaluation/linear_probing/linear_classifier.py
+++ evaluation/linear_probing/linear_classifier.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import SGDClassifier
 from metrics import eval_metrics
-
+import numpy as np
+from typing import List
 
 class LinearProber:
 
@@ -8,11 +9,13 @@ class LinearProber:
         self.alpha = alpha
         self.seed = seed
 
-    def train_and_test(self, train_x, train_y, test_x, test_y):
+    def train_and_test(self, train_x: List, train_y: List, test_x: List, test_y: List):
         classifier = SGDClassifier(random_state=self.seed, loss="log_loss",
                                    alpha=self.alpha, verbose=0,
                                    penalty="l2", max_iter=10000)
 
+        train_y = np.array(train_y).reshape(1, -1)
+        test_y = np.array(test_y).reshape(1, -1)
         classifier.fit(train_x, train_y)
         test_pred = classifier.predict(test_x)
         train_pred = classifier.predict(test_y)
