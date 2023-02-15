4d9cb95d1f7248c9c5b3ad7b673da476ef032e6a
vinid <chiccobia@gmail.com>
2023-02-15 13:18:16 -0800

    adding plip

--- embedders/factory.py
+++ embedders/factory.py
@@ -11,12 +11,12 @@ class EmbedderFactory:
     def factory(self, name, path):
         if name == "plip":
             device = "cuda" if torch.cuda.is_available() else "cpu"
-            model, preprocess = clip.load(name, device=device)
+            model, preprocess = clip.load("ViT-B/32", device=device)
             model.load_state_dict(path, map_location=device)
             return CLIPEmbedder(model, preprocess, name, path)
 
         elif name == "clip":
             device = "cuda" if torch.cuda.is_available() else "cpu"
-            model, preprocess = clip.load(name, device=device)
+            model, preprocess = clip.load("ViT-B/32", device=device)
 
             return CLIPEmbedder(model, preprocess, name, path)
