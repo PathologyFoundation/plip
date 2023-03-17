66baea50003dad8fc1ab8b36c3efa9700732c944
mertyg <merty@stanford.edu>
2023-03-17 15:36:19 -0700

    bash script to reproduce exps

---
+++ reproduce.sh
@@ -0,0 +1,23 @@
+cd scripts
+datasets=("BreaKHis_8_subtypes" "BreaKHis_binary" "DigestPath" "WSSS4LUAD_binary" "Kather" "PanNuke")
+
+# Reproducing linear probing experiments
+# Iterate over different datasets and regularization strengths
+for dataset in "${datasets[@]}";
+do
+  for alpha in 0.0001 0.001 0.01 0.1;
+  do
+    python3 linear_probing_evaluation.py --model_name="plip" --alpha=$alpha --dataset $dataset
+    python3 linear_probing_evaluation.py --model_name="mudipath" --alpha=$alpha --dataset $dataset
+    python3 linear_probing_evaluation.py --model_name="clip" --alpha=$alpha --dataset $dataset
+  done
+done
+
+# Reproducing zero-shot experiments
+
+# TODO
+
+# Reproducing text-to-image retrieval experiments
+
+# TODO
+
